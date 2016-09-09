0a3a5319c29336ab8ee67a56e9cee72754d9fb85
georgecr0ss <george.krystew@gmail.com>
2016-09-09 11:52:19 +0300

    creating class Quater

---
+++ js/app.js
@@ -0,0 +1,8 @@
+var VendigMacihne = (function () {
+    function VendigMacihne() {
+    }
+    return VendigMacihne;
+}());
+/// <reference path="vendingMachine.ts" />
+var machine = new VendigMacihne();
+//# sourceMappingURL=app.js.map
---
+++ ts/coin.ts
@@ -0,0 +1,7 @@
+class Quater {
+    value:number = .25;
+    getImageUrl (): string {
+        return "image/Quater.png"
+    }
+
+}
